7abf32f0cdc95fac554a3367175fa205920ca843
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2022-11-12 23:50:12 +0300

    11-11-2022
    Добавили отображение категорий на странице + сделали новую страницу создания категорий

---
+++ ShopM4/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopM4.Data;
+using ShopM4.Models;
+
+namespace ShopM4.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = db.Category;
+
+            return View(categories);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
+
