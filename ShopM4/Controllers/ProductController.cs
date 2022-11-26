821060fefc0120d5cdd8697b67bd21e8d69751f1
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2022-11-26 23:51:28 +0300

    26.11.22 ViewModel

--- ShopM4/Controllers/ProductController.cs
+++ ShopM4/Controllers/ProductController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ShopM4.Data;
 using ShopM4.Models;
+using ShopM4.Models.ViewModels;
 
 namespace ShopM4.Controllers
 {
@@ -22,28 +24,59 @@ namespace ShopM4.Controllers
         {
             IEnumerable<Product> objList = db.Product;
 
+            // получаем ссылки на сущности категорий
+            foreach (var item in objList)
+            {
+                // сопоставление таблицы категорий и таблицы product
+                item.Category = db.Category.FirstOrDefault(x => x.Id == item.CategoryId);
+            }
+
             return View(objList);
         }
 
         // GET - CreateEdit
         public IActionResult CreateEdit(int? id)
         {
-            Product product = new Product();
+            /*
+            // получаем лист категорий для отправки его во View
+            IEnumerable<SelectListItem> CategoriesList = db.Category.Select(x =>
+                new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                });
+
+            // отправляем лист категорий во View
+            //ViewBag.CategoriesList = CategoriesList;
+            ViewData["CategoriesList"] = CategoriesList;
+            */
+
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Product = new Product(),
+                CategoriesList = db.Category.Select(x =>
+                new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
 
             if (id == null)
             {
                 // create product
-                return View(product);
+                return View(productViewModel);
             }
             else
             {
                 // edit product
-                product = db.Product.Find(id);
-                if (product == null)
+                productViewModel.Product = db.Product.Find(id);
+
+                if (productViewModel.Product == null)
                 {
                     return NotFound();
                 }
-                return View(product);
+                return View(productViewModel);
             }
         }
     }
---
+++ ShopM4/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ShopM4.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+
+        public IEnumerable<SelectListItem> CategoriesList { get; set; }
+    }
+}
+
